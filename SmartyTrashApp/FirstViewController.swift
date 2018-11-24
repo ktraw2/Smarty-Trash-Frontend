34157e020ea2b258f1765688e74a870e227d721f
Priyanka P Mishra <ppmishra@ucsd.edu>
2018-11-23 20:35:33 -0800

    Initial Commit

---
+++ SmartyTrashApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmartyTrashApp
+//
+//  Created by Priyanka Priyadarshini Mishra on 11/23/18.
+//  Copyright © 2018 Priyanka Priyadarshini Mishra. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
